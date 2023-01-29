7fed00823089516687a9b52324aa676fc670bba6
Elmer Bulthuis <elmerbulthuis@gmail.com>
2023-01-29 09:11:07 +0100

    adds comments!

--- src/Route.cs
+++ src/Route.cs
@@ -1,7 +1,17 @@
 namespace goodrouter;
 
+/// <summary>
+/// This is a route...
+/// </summary>
 public class Route
 {
+    /// <summary>
+    /// Name of a route
+    /// </summary>
     public readonly string name;
+
+    /// <summary>
+    /// parameters of a route
+    /// </summary>
     public readonly Dictionary<string, string> parameters;
 }
--- src/Router.cs
+++ src/Router.cs
@@ -1,10 +1,8 @@
 namespace goodrouter;
 
-/*
-<summary>
-This is the router!
-</summary>
-*/
+/// <summary>
+/// This is the router!
+/// </summary>
 public class Router
 {
     private RouteNode rootNode = new RouteNode();
